8452db6452929b7d4a9cfe97eafe946852b60bde
JOB <1295263075@qq.com>
2018-06-04 14:33:41 +0800

    方法的重载，方法名相同，参数列表不同，返回值不能做为重载的条件

--- Array_Test.java
+++ Array_Test.java
@@ -7,5 +7,18 @@ public class Array_Test {
 	//命名：驼峰命名法
 	//形参：方法定义时的参数称为形参
 	//实参：在调用方法时传入的参数称为实参
-
+	//方法体就是完成功能的代码
+    //方法的重载:方法名相同，参数列表不同
+	//返回值不能作为重载的条件
+	public static void main(String[] args) {
+		int result = add (10,20);
+		System.out.println(result);
+	}
+	public static int add(int a ,int b) {
+		return a+b;
+	}
+	public static float add(float a ,float b) {
+		return a+b;
+	}
 }
+
