c2b7b20be1165b4dea40cacc21612984f018a078
JOB <1295263075@qq.com>
2018-06-04 14:54:25 +0800

    数组的定义以及写法

--- Array_Test.java
+++ Array_Test.java
@@ -13,6 +13,23 @@ public class Array_Test {
 	public static void main(String[] args) {
 		int result = add (10,20);
 		System.out.println(result);
+		//数组：一组能够存储相同数据类型的变量的集合
+		//四种方法定义一个数组
+		//数组一定要有长度，如何数组都有长度
+		//数组中的每一个数据称为元素，数组元素的位置从零开始
+		//第一种
+		int[] nums = new int[50];
+//		nums[0] = 1;
+		for(int i = 0 ;i<50;i++) {
+			nums[i] = i+1;
+			//第二种
+			int[] num2 ;
+			num2 = new int[50];
+			//第三种
+			int [] nums3 = new int [] {1,2,3,4};
+			//第四种
+			int [] num4= {1,2,3,4};
+		}
 	}
 	public static int add(int a ,int b) {
 		return a+b;
