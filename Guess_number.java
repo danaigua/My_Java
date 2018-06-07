9304c9763717df74cf2c26e132dae59481323ab2
JOB <1295263075@qq.com>
2018-06-07 16:40:59 +0800

    认识了Java的三维数组，并且知道Java中并没有真正的三位数组，只是一个数组中套一个数组

--- Guess_number.java
+++ Guess_number.java
@@ -5,6 +5,23 @@ import java.util.Random;
 
 public class Guess_number {
 public static void main(String[] args) {
+	//多维数组，Java中没有真正的多维数组，多维数组中的表示方式是数组中的元素还是多维数组
+	/*
+	 * 78   23   43
+	 * 32   43   54
+	 * 45   54   32
+	 */
+	int[] [] scores = {{78,23,43},{32,43,54},{45,54,32}};
+	int classLen = scores.length;
+	for(int i=0;i<classLen;i++) {
+		int sum = 0;
+		int count  = scores[i].length;
+		for (int j = 0;j<count;j++) {
+			sum+=scores[i][j];
+		}
+		int avg = sum/count;
+		System.out.println("第"+(i+1)+"班的平均成绩是："+avg);
+	}
 	Scanner input = new Scanner(System.in);
 	//int [] nums = {30,17,6,2,22,27};
 	int [] nums = new int [5];
