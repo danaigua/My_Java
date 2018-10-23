824cd4f4ad63e361986b92f85da42257a3ec98ac
JOB <1295263075@qq.com>
2018-10-23 19:37:19 +0800

    学习了两种使用线程的方法，Runnable比较好一点

--- _10_22_Thread_use.java
+++ _10_22_Thread_use.java
@@ -2,14 +2,29 @@ package JOB;
 
 public class _10_22_Thread_use {
 	public static void main(String[] args) {
-		
+		MyThread mt = new MyThread();
+		mt.start();//启动线程
+		MyRunnable mr = new MyRunnable();
+		Thread t2 = new Thread(mr);
+		t2.start();
+
 	}
 
 }
-class MyThread extends Thread{
+//实现线程的第一种方式：继承Thread类
+class MyThread extends Thread {
 	@Override
 	public void run() {
-			for()
+			for(int i = 0;i < 50; i++) {
+				System.out.println(Thread.currentThread().getName()+"->"+i);
+			}
 		}
+}
+class MyRunnable implements Runnable{
+	public void run() {
+		for(int i = 0;i < 50; i++) {
+			System.out.println(Thread.currentThread().getName()+"->"+i);
+		}
+		
 	}
 }
